5abb4099e507d4997d3d5c19f53790b0881f19ff
matowSDE <68979618+matowSDE@users.noreply.github.com>
2020-07-30 04:16:47 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,48 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+
+client.on("ready", () => {
+let channel =     client.channels.get("732381969970757652")
+setInterval(function() {
+channel.send(`لحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client2.on("ready", () => {
+let channel =     client2.channels.get("732381969970757652")
+setInterval(function() {
+channel.send(`لحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client3.on("ready", () => {
+let channel =     client3.channels.get("732381969970757652")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client4.on("ready", () => {
+let channel =     client4.channels.get("732381969970757652")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+client5.on("ready", () => {
+let channel =     client5.channels.get("732381969970757652")
+setInterval(function() {
+channel.send(`الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال الحمدلله علي كل حال`);
+}, 30)
+})
+
+
+client.login(process.env.T);
+client2.login(process.env.T2);
+client3.login(process.env.T3);
+client4.login(process.env.T4);
+client5.login(process.env.T5);
